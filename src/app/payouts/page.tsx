e74a2af0c50b979333e6ad33398e6b7ab54f6155
Nick Tavares <nick@macbookpro.mynetworksettings.com>
2025-07-07 17:03:34 -0400

    Fix: Provide required props to PayoutTable in payouts page

--- src/app/payouts/page.tsx
+++ src/app/payouts/page.tsx
@@ -1,5 +1,6 @@
 import PayoutTable from "@/components/PayoutTable";
 
 export default function PayoutsPage() {
-  return <PayoutTable />;
+  // TODO: Replace with real values or fetch from your data source
+  return <PayoutTable prizePool={1000} payoutsCount={3} />;
 } 
